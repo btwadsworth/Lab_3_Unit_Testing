b6108483651475732388cc1ef161651e52c7a858
Ben Wadsworth <btwadsworth@gmail.com>
2020-02-04 12:13:10 -0600

    Finished TODO in phone_info()

--- phone_manager.py
+++ phone_manager.py
@@ -80,16 +80,15 @@ class PhoneAssignments():
 
     def phone_info(self, employee):
         # find phone for employee in phones list
-
         # TODO  should return None if the employee does not have a phone
         # TODO  the method should raise an exception if the employee does not exist
-
-        for phone in self.phones:
-            if phone.employee_id == employee.id:
-                return phone
-
-
-        return None
+        if employee not in self.employees:
+            raise Exception('Employee does not exist')
+        else:
+            for phone in self.phones:
+                if phone.employee_id == employee.id:
+                    return phone
+            return None
 
 
 class PhoneError(Exception):
